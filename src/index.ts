1ff0523d5baec0bc8f9ce432a260e62e03fa2f1e
tettekete <6535634+tettekete@users.noreply.github.com>
2024-08-17 23:49:36 +0900

    update: axios による API コール直前までを実装

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
+import url from 'node:url';
+import { loadCredential } from './util/credential-loader';
+import { getLogger } from './util/logger';
+import ExURL from './util/exUrl';
 
-let hello: string = 'Hello World！'
-console.log(hello)
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+
+const lg = getLogger("trace");
+// config.yaml を読み込む
+const creds = loadCredential('credentials.yaml');
+
+// クレデンシャル情報の使用例
+lg.trace(`user: ${creds.user}`);
+lg.trace(`password: ${creds.password}`);
+
+//
+const api_base_url = new ExURL('https://api.jquants.com/v1/');
+
+const refresh_api = api_base_url.withPath( 'token/auth_user' );
+refresh_api.method = "POST";
+
+lg.trace(`api_base_url: ${api_base_url.toString()}`);
+lg.trace(`refresh_api: ${refresh_api.toString()}`);
+
+const req: AxiosRequestConfig =
+{
+	url:	refresh_api.toString(),
+	method: refresh_api.method,
+	data:
+	{
+		mailaddress: creds.user,
+		password: creds.password
+	}
+}

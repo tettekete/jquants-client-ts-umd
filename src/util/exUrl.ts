60d80cb7c725dfb207c25c5d78546e184f8f8e64
tettekete <6535634+tettekete@users.noreply.github.com>
2024-08-17 23:44:57 +0900

    add: util/exUrls.ts を追加しました。
    URL 派生クラス。
    標準のままだと API URL のベース URL と、API パスを追加した URL を
    使うのがやりづらかったため作成。
    
    - clone メソッドがないのでベース URL を書き換えてしまう
    - パスの結合メソッドがないので /v1/ というベースパスに `auth` を追加する、
      といった処理が面倒
    - ついでにリクエストメソッドを覚えておけるように拡張

---
+++ src/util/exUrl.ts
@@ -0,0 +1,49 @@
+
+type HTTP_METHODS_T = "GET" 
+					| "HEAD"
+					| "POST"
+					| "PUT"
+					| "DELETE"
+					| "CONNECT"
+					| "TRACE"
+					| "PATCH"
+					| "OPTIONS"
+
+export default class ExUrl extends URL {
+	protected _method: HTTP_METHODS_T = "GET"
+
+	constructor(url: string, base?: string) {
+		super(url, base);
+	}
+
+	set method( _method: HTTP_METHODS_T )
+	{
+		this._method = _method;
+	}
+
+	get method(): HTTP_METHODS_T
+	{
+		return this._method
+	}
+
+	// clone メソッドを追加して新しい URL インスタンスを返す
+	public clone(): ExUrl {
+		const cloned = new ExUrl(this.toString());
+		cloned.method = this.method;
+		return cloned;
+	}
+
+	// withPath メソッドを追加してパスを追加した新しい URL を返す
+	public withPath(path: string): ExUrl {
+		const cloned = this.clone();
+		if( ! cloned.pathname.match(/\/$/) )
+		{
+			cloned.pathname += '/';
+		}
+
+		path = path.replace(/^\//,'');
+
+		cloned.pathname = new URL(path, cloned).pathname;
+		return cloned;
+	}
+}
